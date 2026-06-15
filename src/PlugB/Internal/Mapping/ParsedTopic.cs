0e2b6190f65d12723945fe41ea3ec960dccaa789
Philipp Siewert <philipp@phsiewert.de>
2026-06-15 08:52:14 +0200

    add topic parsing

---
+++ src/PlugB/Internal/Mapping/ParsedTopic.cs
@@ -0,0 +1,8 @@
+﻿namespace PlugB.Internal.Mapping;
+
+internal record ParsedTopic(
+    SparkplugMessageKind Kind,
+    string? GroupId,
+    string? EdgeNodeId,
+    string? DeviceId,
+    string? HostId);
